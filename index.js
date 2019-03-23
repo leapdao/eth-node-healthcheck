07c3527bfee1385b5c67f636fa6662cb58a52305
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2019-03-23 18:18:41 +0100

    cleanup

--- index.js
+++ index.js
@@ -2,46 +2,34 @@
 
 const ethers = require('ethers');
 const http = require('http');
-const https = require('https');
-const { exec } = require('child_process');
 
+const host = process.env.RPC_HOST || 'localhost';
 const provider = ethers.getDefaultProvider(process.env.NETWORK);
+const localProvider = new ethers.providers.JsonRpcProvider(`http://${host}:8545`);
 const MAX_BLOCK_DIFFERENCE = 3;
 
-const getLocalBlockNum = () => {
-  const host = process.env.RPC_HOST || 'localhost';
-  return new Promise((resolve, reject) => {
-    exec(`curl --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST ${host}:8545`, (error, stdout, stderr) => {
-      if (error) {
-        return reject(`${error}`);
-      }
-      resolve(parseInt(JSON.parse(stdout.trim()).result));
-    });
-  });
-};
-
-const getNetworkBlockNum = async () => {
-  return await provider.getBlockNumber();
-};
-
-const onHealthcheckRequest = (req, res) => {
+const onHealthcheckRequest = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-  Promise.all([getLocalBlockNum(), getNetworkBlockNum()])
-    .then((values) => {
-      const [ localBlockNum, networkBlockNum ] = values;
-      let responseStatus = networkBlockNum - localBlockNum > MAX_BLOCK_DIFFERENCE ? 500 : 200;
-      if (localBlockNum > 10000 && networkBlockNum <= 0) { // don't let etherscan f**k us
-        responseStatus = 200;
-      }
-      res.writeHead(responseStatus, { 'Content-Type': 'text/plain' });
-      res.end((localBlockNum - networkBlockNum).toString());
-    }).catch(e => {
-      console.error(e);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(e);
-    });
+  let localBlockNum;
+  let networkBlockNum;
+
+  try {
+    localBlockNum = await localProvider.getBlockNumber();
+    networkBlockNum = await provider.getBlockNumber();
+  } catch (e) {
+    console.error(e);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end(e);
+  }
+
+  let responseStatus = networkBlockNum - localBlockNum > MAX_BLOCK_DIFFERENCE ? 500 : 200;
+  if (localBlockNum > 10000 && networkBlockNum <= 0) { // don't let etherscan f**k us
+    responseStatus = 200;
+  }
+  res.writeHead(responseStatus, { 'Content-Type': 'text/plain' });
+  res.end((localBlockNum - networkBlockNum).toString());
 };
 
 http.createServer(onHealthcheckRequest).listen(process.env.PORT);
