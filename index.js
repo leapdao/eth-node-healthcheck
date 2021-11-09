f7a7f28248045ed57c028db00b2b6c3f2c1fafe9
Isman Usoh <isman.isman@bitkub.com>
2021-11-09 17:48:09 +0700

    fix: support json provider

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const ethers = require('ethers');
 const http = require('http');
 
-const url = process.env.RPC_URL || 'http://localhost:8545';
 const port = process.env.PORT || 80
-const provider = ethers.getDefaultProvider(process.env.NETWORK);
+const url = process.env.RPC_URL || 'http://localhost:8545';
+const network = process.env.NETWORK_URL || 'https://rpc.bitkubchain.io';
+
+const provider = new ethers.providers.JsonRpcProvider(network);
 const localProvider = new ethers.providers.JsonRpcProvider(url);
 const MAX_BLOCK_DIFFERENCE = process.env.MAX_BLOCK_DIFFERENCE || 50;
 
