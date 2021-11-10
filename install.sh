09cf5a1645dda102180288722b362b38639da8cf
Isman Usoh <isman.isman@bitkub.com>
2021-11-10 23:48:32 +0700

    fix: install script

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 
 sudo su -
 
-curl https://github.com/isman-usoh/eth-node-healthcheck/raw/master/eth-node-healthcheck-linux-x64 --output /usr/bin/eth-node-healthcheck-linux-x64
-curl https://github.com/isman-usoh/eth-node-healthcheck/raw/master/services/eth-node-healthcheck.service --output /etc/systemd/system/eth-node-healthcheck.service
+curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/eth-node-healthcheck-linux-x64 --output /usr/bin/eth-node-healthcheck-linux-x64
+curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/services/eth-node-healthcheck.service --output /etc/systemd/system/eth-node-healthcheck.service
 chmod +x /usr/bin/eth-node-healthcheck-linux-x64
-
 
 systemctl daemon-reload
 systemctl enable eth-node-healthcheck.service
