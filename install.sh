3506f166cb0c03638fbc805ec0b6ab3e670e75df
Isman Usoh <isman.usoh@gmail.com>
2022-10-17 14:38:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 
 sudo su -
 
-curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/eth-node-healthcheck-linux-x64 --output /usr/bin/eth-node-healthcheck-linux-x64
-curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/services/eth-node-healthcheck.service --output /etc/systemd/system/eth-node-healthcheck.service
-chmod +x /usr/bin/eth-node-healthcheck-linux-x64
+curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/bkc-node-healthcheck-linux-x64 --output /usr/bin/bkc-node-healthcheck-linux-x64
+curl https://raw.githubusercontent.com/isman-usoh/eth-node-healthcheck/master/services/eth-node-healthcheck.service --output /etc/systemd/system/bkc-node-healthcheck.service
+chmod +x /usr/bin/bkc-node-healthcheck-linux-x64
 
 systemctl daemon-reload
-systemctl enable eth-node-healthcheck.service
-systemctl start eth-node-healthcheck.service
-systemctl status eth-node-healthcheck.service
+systemctl enable bkc-node-healthcheck.service
+systemctl start bkc-node-healthcheck.service
+systemctl status bkc-node-healthcheck.service
